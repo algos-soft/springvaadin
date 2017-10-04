607daf7f9e4c4cec1a81411e95f7b86210ea66a4
GuidoCeresa <gac@algos.it>
2017-10-04 19:03:11 +0200

    ...

--- src/main/java/it/algos/springvaadin/lib/LibAvviso.java
+++ src/main/java/it/algos/springvaadin/lib/LibAvviso.java
@@ -14,13 +14,45 @@ public abstract class LibAvviso {
         show(message,Notification.Type.WARNING_MESSAGE);
     }// end of static method
 
+    public static void warnAdmin(String message) {
+        showAdmin(message,Notification.Type.WARNING_MESSAGE);
+    }// end of static method
+
+    public static void warnDev(String message) {
+        showDev(message,Notification.Type.WARNING_MESSAGE);
+    }// end of static method
+
     public static void error(String message) {
         show(message,Notification.Type.ERROR_MESSAGE);
+    }// end of static method
+
+    public static void errorAdmin(String message) {
+        showAdmin(message,Notification.Type.ERROR_MESSAGE);
+    }// end of static method
+
+    public static void errorDev(String message) {
+        showDev(message,Notification.Type.ERROR_MESSAGE);
     }// end of static method
 
     private static void show(String message,Notification.Type type) {
         Notification nota = new Notification("Avviso", message, type, true);
         nota.show(Page.getCurrent());
+    }// end of static method
+
+
+    private static void showAdmin(String message,Notification.Type type) {
+        if (LibSession.isAdmin()) {
+            Notification nota = new Notification("Avviso", message, type, true);
+            nota.show(Page.getCurrent());
+        }// end of if cycle
+    }// end of static method
+
+
+    private static void showDev(String message,Notification.Type type) {
+        if (LibSession.isDeveloper()||LibParams.useDebug()) {
+            Notification nota = new Notification("Avviso", message, type, true);
+            nota.show(Page.getCurrent());
+        }// end of if cycle
     }// end of static method
 
 
